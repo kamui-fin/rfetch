8845654e9467fb3cf0a2f460d61961cfacb17b97
Kamui <fin-kamui@pm.me>
2021-03-01 19:50:56 -0600

    Added a few more system info functions

--- src/stats.rs
+++ src/stats.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
-use std::fs;
-
 use bytesize::ByteSize;
+use std::collections::HashMap;
+use std::ffi::CStr;
+use std::fs;
+use std::time::Duration;
 
 pub struct CPUInfo {
     model_name: String,
@@ -14,6 +15,31 @@ pub struct MemInfo<T> {
     avail: T,
     cached: T,
     buffers: T,
+}
+
+pub struct SysInfo {
+    uptime: Duration,
+    process_num: u16,
+}
+
+pub struct UserInfo {
+    name: String,
+    home: String,
+    shell: String,
+}
+
+pub struct MachineInfo {
+    arch: String,
+    kernel: String,
+    hostname: String,
+}
+
+pub struct Color(pub String);
+
+pub struct Distro(pub String);
+
+fn c_ptr_to_string(ptr: *const i8) -> String {
+    unsafe { CStr::from_ptr(ptr).to_owned().into_string().unwrap() }
 }
 
 pub fn get_cpu_info() -> Vec<CPUInfo> {
@@ -51,10 +77,77 @@ pub fn get_mem_info() -> MemInfo<ByteSize> {
         .collect();
 
     MemInfo {
-        total: ByteSize::kb(*mem.get("MemTotal").unwrap()),
-        free: ByteSize::kb(*mem.get("MemFree").unwrap()),
-        avail: ByteSize::kb(*mem.get("MemAvailable").unwrap()),
-        cached: ByteSize::kb(*mem.get("Cached").unwrap()),
-        buffers: ByteSize::kb(*mem.get("Buffers").unwrap()),
+        total: ByteSize::kb(mem["MemTotal"]),
+        free: ByteSize::kb(mem["MemFree"]),
+        avail: ByteSize::kb(mem["MemAvailable"]),
+        cached: ByteSize::kb(mem["Cached"]),
+        buffers: ByteSize::kb(mem["Buffers"]),
+    }
+}
+
+pub fn get_user_info() -> UserInfo {
+    unsafe {
+        let pw_ent = *libc::getpwuid(libc::getuid());
+        UserInfo {
+            name: c_ptr_to_string(pw_ent.pw_name),
+            home: c_ptr_to_string(pw_ent.pw_dir),
+            shell: c_ptr_to_string(pw_ent.pw_shell),
+        }
+    }
+}
+
+pub fn get_machine_info() -> MachineInfo {
+    unsafe {
+        let mut buffer = std::mem::MaybeUninit::<libc::utsname>::uninit();
+        libc::uname(buffer.as_mut_ptr());
+        buffer.assume_init();
+        let buffer = *buffer.as_ptr();
+
+        MachineInfo {
+            arch: c_ptr_to_string(buffer.machine.as_ptr()),
+            kernel: c_ptr_to_string(buffer.release.as_ptr()),
+            hostname: c_ptr_to_string(buffer.nodename.as_ptr()),
+        }
+    }
+}
+
+pub fn get_color_scheme() -> Vec<Color> {
+    let mut colors: Vec<Color> = vec![];
+
+    for i in (30..=37).chain(40..=47) {
+        colors.push(Color(format!("\x1B[{}m", i)));
+    }
+
+    colors
+}
+
+pub fn get_distro() -> Distro {
+    let os_release = fs::read_to_string("/etc/os-release").unwrap();
+    let os_release: HashMap<String, String> = os_release
+        .split("\n")
+        .filter(|line| !line.is_empty())
+        .map(|elm| {
+            let data = elm.split("=").collect::<Vec<&str>>();
+            (data[0].to_string(), data[1].to_string())
+        })
+        .collect();
+
+    Distro(os_release.get("NAME").unwrap().to_string())
+}
+
+pub fn get_sysinfo() -> SysInfo {
+    unsafe {
+        let mut inf = std::mem::MaybeUninit::<libc::sysinfo>::uninit();
+        libc::sysinfo(inf.as_mut_ptr());
+        inf.assume_init();
+        let inf = *inf.as_ptr()
+
+        let uptime = Duration::new(inf.uptime as u64, 0);
+        let process_num = inf.procs;
+
+        SysInfo {
+            uptime,
+            process_num,
+        }
     }
 }
