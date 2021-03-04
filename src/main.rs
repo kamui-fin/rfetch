b019cd796cc2f26379da88b984c85dcd25270d95
Kamui <fin-kamui@pm.me>
2021-03-03 18:34:49 -0600

    fs stats and started on frontend

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-#[macro_use]
-extern crate log;
-
 extern crate bytesize;
 extern crate clap;
-extern crate pretty_env_logger;
 
 use clap::{App, Arg};
 
 mod stats;
-use stats::*;
 
 fn main() {
-    pretty_env_logger::init();
-
     let matches = App::new("rfetch")
         .version("0.1")
         .author("Kamui")
@@ -27,6 +20,49 @@ fn main() {
         )
         .get_matches();
 
-    let conf_file = matches.value_of("config").unwrap_or("config.toml");
-    info!("Config file path: {}", conf_file);
+    let user_info = stats::user_info();
+    let default_config = [
+        user_info.home.clone(),
+        String::from("/.config/rfetch/config.toml"),
+    ]
+    .join("");
+
+    let _conf_file = matches
+        .value_of("config")
+        .unwrap_or(default_config.as_str());
+
+    let distro = stats::distro();
+    let colors = stats::color_scheme();
+    let machine_info = stats::machine_info();
+    let mem = stats::mem_info();
+    let sys_info = stats::sysinfo();
+
+    let fmt_uptime = humantime::format_duration(sys_info.uptime).to_string();
+
+    // TODO: Add ascii art logos
+    // println!(r"    .--.");
+    // println!(r"   |o_o |");
+    // println!(r"   |:_/ |");
+    // println!(r"  //   \ \");
+    // println!(r" (|     | )");
+    // println!(r"/'\_   _/`\");
+    // println!(r"\___)=(___/");
+
+    let user_host = format!("{}@{}", user_info.name, machine_info.hostname);
+    println!("{}", user_host);
+    println!("{}", "-".repeat(user_host.len()));
+    println!("OS:\t{}", distro.name);
+    println!("Kernel:\t{}", machine_info.kernel);
+    println!("Uptime:\t{}", fmt_uptime);
+    println!("Shell:\t{}", user_info.shell);
+    println!("Memory:\t{} / {}", mem.used, mem.total);
+    println!("");
+
+    for (indx, color) in colors.into_iter().enumerate() {
+        if indx == 8 {
+            println!("")
+        }
+        print!("{}  \x1B[0m", color.0,);
+    }
+    println!("");
 }
--- src/stats.rs
+++ src/stats.rs
@@ -1,48 +1,78 @@
 use bytesize::ByteSize;
-use std::collections::HashMap;
+use chrono::prelude::{DateTime, Local};
+use isolang::Language;
 use std::ffi::CStr;
 use std::fs;
 use std::time::Duration;
+use std::{collections::HashMap, ffi::CString};
+
+// TODO:
+// Replace unwraps with proper error handling
+// Re-consider uses of `String` in some scenarios
+// Try to utilize async and multi-threading for better performance
 
 pub struct CPUInfo {
-    model_name: String,
-    cpu_mhz: f64,
+    pub model_name: String,
+    pub cpu_mhz: f64,
 }
 
 pub struct MemInfo<T> {
-    total: T,
-    free: T,
-    avail: T,
-    cached: T,
-    buffers: T,
+    pub total: T,
+    pub avail: T,
+    pub cached: T,
+    pub buffers: T,
+    pub used: T,
 }
 
 pub struct SysInfo {
-    uptime: Duration,
-    process_num: u16,
+    pub uptime: Duration,
+    pub process_num: u16,
 }
-
 pub struct UserInfo {
-    name: String,
-    home: String,
-    shell: String,
+    pub name: String,
+    pub home: String,
+    pub shell: String,
 }
 
 pub struct MachineInfo {
-    arch: String,
-    kernel: String,
-    hostname: String,
+    pub arch: String,
+    pub kernel: String,
+    pub hostname: String,
+}
+
+pub struct Distro {
+    pub name: String,
+    pub color: String,
+}
+
+pub struct LocaleInfo {
+    pub locale: String,
+    pub hr_lang: String,
+}
+
+pub struct FsInfo<T> {
+    pub mount_point: String,
+    pub filesys: String,
+    pub total_size: T,
+    pub free: T,
 }
 
 pub struct Color(pub String);
 
-pub struct Distro(pub String);
+// Utility functions
 
 fn c_ptr_to_string(ptr: *const i8) -> String {
     unsafe { CStr::from_ptr(ptr).to_owned().into_string().unwrap() }
 }
 
-pub fn get_cpu_info() -> Vec<CPUInfo> {
+fn get_env(key: String) -> String {
+    std::env::var(key).unwrap()
+}
+
+// Functions for getting statistics and information about the system
+// Used by the rfetch frontend (might seperate this into another crate)
+
+pub fn cpu_info() -> Vec<CPUInfo> {
     let data = fs::read_to_string("/proc/cpuinfo").unwrap();
     let blocks = data
         .split("\n")
@@ -62,7 +92,7 @@ pub fn get_cpu_info() -> Vec<CPUInfo> {
     cpus
 }
 
-pub fn get_mem_info() -> MemInfo<ByteSize> {
+pub fn mem_info() -> MemInfo<ByteSize> {
     let data = fs::read_to_string("/proc/meminfo").unwrap();
     let mem: HashMap<String, u64> = data
         .split("\n")
@@ -76,16 +106,22 @@ pub fn get_mem_info() -> MemInfo<ByteSize> {
         })
         .collect();
 
+    let total = mem["MemTotal"];
+    let avail = mem["MemAvailable"];
+    let cached = mem["Cached"];
+    let buffers = mem["Buffers"];
+    let used = total - avail;
+
     MemInfo {
-        total: ByteSize::kb(mem["MemTotal"]),
-        free: ByteSize::kb(mem["MemFree"]),
-        avail: ByteSize::kb(mem["MemAvailable"]),
-        cached: ByteSize::kb(mem["Cached"]),
-        buffers: ByteSize::kb(mem["Buffers"]),
+        total: ByteSize::kb(total),
+        avail: ByteSize::kb(avail),
+        cached: ByteSize::kb(cached),
+        buffers: ByteSize::kb(buffers),
+        used: ByteSize::kb(used),
     }
 }
 
-pub fn get_user_info() -> UserInfo {
+pub fn user_info() -> UserInfo {
     unsafe {
         let pw_ent = *libc::getpwuid(libc::getuid());
         UserInfo {
@@ -96,7 +132,7 @@ pub fn get_user_info() -> UserInfo {
     }
 }
 
-pub fn get_machine_info() -> MachineInfo {
+pub fn machine_info() -> MachineInfo {
     unsafe {
         let mut buffer = std::mem::MaybeUninit::<libc::utsname>::uninit();
         libc::uname(buffer.as_mut_ptr());
@@ -111,17 +147,14 @@ pub fn get_machine_info() -> MachineInfo {
     }
 }
 
-pub fn get_color_scheme() -> Vec<Color> {
-    let mut colors: Vec<Color> = vec![];
-
-    for i in (30..=37).chain(40..=47) {
-        colors.push(Color(format!("\x1B[{}m", i)));
-    }
-
-    colors
+pub fn color_scheme() -> Vec<Color> {
+    (40..=47)
+        .chain(100..=107)
+        .map(|cl| Color(format!("\x1B[{}m", cl)))
+        .collect::<Vec<Color>>()
 }
 
-pub fn get_distro() -> Distro {
+pub fn distro() -> Distro {
     let os_release = fs::read_to_string("/etc/os-release").unwrap();
     let os_release: HashMap<String, String> = os_release
         .split("\n")
@@ -132,10 +165,13 @@ pub fn get_distro() -> Distro {
         })
         .collect();
 
-    Distro(os_release.get("NAME").unwrap().to_string())
+    Distro {
+        name: os_release["NAME"].clone(),
+        color: os_release["ANSI_COLOR"].clone(),
+    }
 }
 
-pub fn get_sysinfo() -> SysInfo {
+pub fn sysinfo() -> SysInfo {
     unsafe {
         let mut inf = std::mem::MaybeUninit::<libc::sysinfo>::uninit();
         libc::sysinfo(inf.as_mut_ptr());
@@ -151,3 +187,65 @@ pub fn get_sysinfo() -> SysInfo {
         }
     }
 }
+
+pub fn current_datetime() -> DateTime<Local> {
+    Local::now()
+}
+
+pub fn locale() -> LocaleInfo {
+    let locale = get_env(String::from("LANG"));
+    let hr_lang = Language::from_locale(locale.as_str())
+        .unwrap()
+        .to_name()
+        .to_string();
+    LocaleInfo { locale, hr_lang }
+}
+
+pub fn disk_usage() -> Vec<FsInfo<ByteSize>> {
+    unsafe {
+        let mut disk_stats: Vec<FsInfo<ByteSize>> = vec![];
+
+        let mtab = CString::new("/etc/mtab").unwrap();
+        let perm = CString::new("r").unwrap();
+        let mnts = libc::setmntent(mtab.as_ptr(), perm.as_ptr());
+
+        let mut mntent = libc::getmntent(mnts);
+
+        while !mntent.is_null() {
+            let mnt_pnt = (*mntent).mnt_dir;
+            let filesys = (*mntent).mnt_type;
+            let mut stat_buf = std::mem::MaybeUninit::<libc::statfs>::uninit();
+            libc::statfs(mnt_pnt, stat_buf.as_mut_ptr());
+            stat_buf.assume_init();
+            let stat_buf = *stat_buf.as_ptr();
+            let free = stat_buf.f_bsize as u64 * stat_buf.f_bavail;
+            let total = stat_buf.f_bsize as u64 * stat_buf.f_blocks;
+
+            disk_stats.push(FsInfo {
+                mount_point: c_ptr_to_string(mnt_pnt),
+                filesys: c_ptr_to_string(filesys),
+                total_size: ByteSize::b(total),
+                free: ByteSize::b(free),
+            });
+
+            mntent = libc::getmntent(mnts);
+        }
+
+        libc::endmntent(mnts);
+        disk_stats
+    }
+}
+
+// A few more feature ideas
+
+// pub fn packages() {}
+// pub fn gpu_info() {}
+// pub fn default_apps() {}
+// pub fn current_track() {}
+// pub fn wm_name() {}
+// pub fn theme_info() {}
+// pub fn display_info() {}
+// pub fn init_system() {}
+// pub fn volume() {}
+// pub fn ip() {}
+// pub fn in_speed() {}
